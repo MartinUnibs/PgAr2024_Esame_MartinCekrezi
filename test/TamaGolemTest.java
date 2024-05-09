69e2c627ad5bcbdf0efa0d6a89de8240997d4509
Motty <m.mottinelli002@studenti.unibs.it>
2024-05-09 21:48:02 +0200

    JUnit setup

---
+++ test/TamaGolemTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TamaGolemTest {
+
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+        //TODO: tests
+    }
+
+}
