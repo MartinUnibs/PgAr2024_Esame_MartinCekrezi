d88c9965584a259d6620bed9f4f146bbea7845c4
Motty <m.mottinelli002@studenti.unibs.it>
2024-05-13 22:58:11 +0200

    feat: hard coded matrix

--- src/it/unibs/arnaldo/tamagolem/graph/BalanceManager.java
+++ src/it/unibs/arnaldo/tamagolem/graph/BalanceManager.java
@@ -4,11 +4,17 @@ package it.unibs.arnaldo.tamagolem.graph;
 public class BalanceManager {
     //TODO: everything
 
-    private int[][] graph;
+    private int[][] graph = { //TODO: to remove
+            {0, 1, -4, -6, 9},
+            {-1, 0, -9, 9, 1},
+            {4, 9, 0, -10, -3},
+            {6, -9, 10, 0, -7},
+            {-9, -1, 3, 7, 0},
+    };
 
     public BalanceManager(int nodes) {
         //TODO: da fare
-        graph = ; //TODO: init (hard code)
+        //this.graph = BalanceGenerator.generateBalance(nodes);
     }
 
     public int getDamage(int p1, int p2) {
