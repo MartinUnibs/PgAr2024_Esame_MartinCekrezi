51d1d738b4cb615da93d06192e158e0c8954e36e
Motty <m.mottinelli002@studenti.unibs.it>
2024-05-13 22:46:23 +0200

    refactor: fixed welcome and farewell

--- src/it/unibs/arnaldo/tamagolem/main/TamaGolemMain.java
+++ src/it/unibs/arnaldo/tamagolem/main/TamaGolemMain.java
@@ -6,14 +6,14 @@ import it.unibs.arnaldo.tamagolem.game.Duel;
 public class TamaGolemMain {
 
     public static void main(String[] args) {
-        //TODO: WELCOME
+        UserInteraction.welcome();
         boolean choice = true;
         do {
-            int difficulty = UserInteraction.chooseDifficulty(); //TODO: scegli livello difficolta
+            int difficulty = UserInteraction.chooseDifficulty();
             Duel duel = new Duel(difficulty);
             duel.start();
             choice = false; //TODO: scelta utente se vuole continuare
         } while (choice);
-        //TODO: FAREWELL
+        UserInteraction.farewell();
     }
 }
