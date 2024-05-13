c1231a68cdc9c052e0240ed4900e05a6fb17192b
Motty <m.mottinelli002@studenti.unibs.it>
2024-05-13 22:52:44 +0200

    prova git

--- src/it/unibs/arnaldo/tamagolem/graph/BalanceManager.java
+++ src/it/unibs/arnaldo/tamagolem/graph/BalanceManager.java
@@ -4,8 +4,11 @@ package it.unibs.arnaldo.tamagolem.graph;
 public class BalanceManager {
     //TODO: everything
 
+    private int[][] graph;
+
     public BalanceManager(int nodes) {
         //TODO: da fare
+        graph = ; //TODO: init (hard code)
     }
 
     public int getDamage(int p1, int p2) {
