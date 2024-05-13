5a2da99dc0c7d0866c07b3c2be7225cbf5f1395d
Nekoboss <cekrezimartin@gmail.com>
2024-05-13 20:09:57 +0200

    not committato bene

--- src/it/unibs/arnaldo/tamagolem/game/Player.java
+++ src/it/unibs/arnaldo/tamagolem/game/Player.java
@@ -1,9 +1,13 @@
 package it.unibs.arnaldo.tamagolem.game;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
 
 //TODO: DOC
 public class Player {
     //TODO: everything here
     Deque <TamaGolem> roster;
+    public Player() {
+        roster = new ArrayDeque<TamaGolem>();
+    }
 }
