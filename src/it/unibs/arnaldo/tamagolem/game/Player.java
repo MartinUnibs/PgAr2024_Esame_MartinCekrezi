00cb96d73bfbd08d1c4e50d6d48d71245e5cabb2
Nekoboss <cekrezimartin@gmail.com>
2024-05-13 20:17:49 +0200

    comments

--- src/it/unibs/arnaldo/tamagolem/game/Player.java
+++ src/it/unibs/arnaldo/tamagolem/game/Player.java
@@ -6,8 +6,23 @@ import java.util.Deque;
 //TODO: DOC
 public class Player {
     //TODO: everything here
+
+    //TODO: String name per il giocatore?
     Deque <TamaGolem> roster;
-    public Player() {
+
+    public Player(int nTamagolem, int stonePerTamaGolem) {
+
         roster = new ArrayDeque<TamaGolem>();
+
+        for(int i=0; i < nTamagolem; i++){
+            //TODO: Nome per il tamagolem?
+            //Idea: Elemento + nome arnaldo random, " Kibo fiamma " oppure " Phoenix etereo " ecc
+            roster.push(new TamaGolem(/* stonePerTamaGolem */));
+        }
     }
+
+    public Deque<TamaGolem> getRoster() {
+        return roster;
+    }
+
 }
