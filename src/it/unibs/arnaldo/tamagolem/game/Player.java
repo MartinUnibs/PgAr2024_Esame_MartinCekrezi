e7c3bc524f15e8812490d4e5a64e3a8bd2437a8b
Nekoboss <cekrezimartin@gmail.com>
2024-05-13 20:03:57 +0200

    Player constructor

--- src/it/unibs/arnaldo/tamagolem/game/Player.java
+++ src/it/unibs/arnaldo/tamagolem/game/Player.java
@@ -1,6 +1,16 @@
 package it.unibs.arnaldo.tamagolem.game;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 //TODO: DOC
 public class Player {
-    //TODO: everything here
+
+    Deque<TamaGolem> roster;
+
+    public Player() {
+        roster = new ArrayDeque<TamaGolem>();
+    }
+
 }
+
